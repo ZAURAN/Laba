fb39d393fad8a5333fe17ebd21aa7b7b2541e86c
ZAURAN <75642580+ZAURAN@users.noreply.github.com>
2024-03-17 00:49:51 +0200

    Add files via upload

---
+++ pr_3/task 2/task 2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+
+/* --- Клас Point
+public class Point
+{
+    private int x;
+    private int y;
+    private string pointName;
+
+    public int X
+    {
+        get { return x; }
+    }
+
+    public int Y
+    {
+        get { return y; }
+    }
+
+    public string PointName
+    {
+        get { return pointName; }
+    }
+
+    public Point(int x, int y, string pointName)
+    {
+        this.x = x;
+        this.y = y;
+        this.pointName = pointName;
+    }
+}
+*/
+
+
+/* --- Клас Figure
+using System.Drawing;
+
+public class Figure
+{
+    private Point[] points;
+
+    public Figure(Point a, Point b, Point c)
+    {
+        points = new Point[] { a, b, c };
+    }
+
+    public Figure(Point a, Point b, Point c, Point d)
+    {
+        points = new Point[] { a, b, c, d };
+    }
+
+    public Figure(Point a, Point b, Point c, Point d, Point e)
+    {
+        points = new Point[] { a, b, c, d, e };
+    }
+
+    private double LengthSide(Point A, Point B)
+    {
+        return Math.Sqrt(Math.Pow((B.X - A.X), 2) + Math.Pow((B.Y - A.Y), 2));
+    }
+
+    public void PerimeterCalculator()
+    {
+        double perimeter = 0;
+        for (int i = 0; i < points.Length; i++)
+        {
+            if (i == points.Length - 1)
+            {
+                perimeter += LengthSide(points[i], points[0]);
+            }
+            else
+            {
+                perimeter += LengthSide(points[i], points[i + 1]);
+            }
+        }
+
+        Console.WriteLine($"Периметр багатокутника: {perimeter}");
+    }
+}
+*/
+
+
+
+/* --- Програма для демонстрації
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point a = new Point(0, 0, "A");
+        Point b = new Point(4, 0, "B");
+        Point c = new Point(4, 3, "C");
+
+        Figure triangle = new Figure(a, b, c);
+        Console.WriteLine("Трикутник:");
+        triangle.PerimeterCalculator();
+
+        Point d = new Point(0, 3, "D");
+        Figure rectangle = new Figure(a, b, c, d);
+        Console.WriteLine("Прямокутник:");
+        rectangle.PerimeterCalculator();
+    }
+}
+*/
---
+++ pr_3/task1/task1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+class Book
+{
+    public Title Title { get; }
+    public Author Author { get; set; }
+    public Content Content { get; set; }
+
+    public Book(string title)
+    {
+        this.Title = new Title(title);
+    }
+
+    public void Show()
+    {
+        Title.Show();
+        Author.Show();
+        Content.Show();
+    }
+}
+
+class Title
+{
+    private string title;
+
+    public Title(string title)
+    {
+        this.title = title;
+    }
+
+    public void Show()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Назва: " + title);
+        Console.ResetColor();
+    }
+}
+
+class Author
+{
+    public string Name { get; set; }
+
+    public Author(string name)
+    {
+        Name = name;
+    }
+
+    public void Show()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Автор: " + Name);
+        Console.ResetColor();
+    }
+}
+
+class Content
+{
+    public string Info { get; set; }
+
+    public Content(string info)
+    {
+        Info = info;
+    }
+
+    public void Show()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Зміст: " + Info);
+        Console.ResetColor();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Book book = new Book("Історія України");
+        book.Author = new Author("Іван Франко");
+        book.Content = new Content("Ця книга розповідає про історію України...");
+
+        book.Show();
+    }
+}
